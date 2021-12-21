fc7d334e01a7e39928467f63cb325c8302ba46d3
Charlotte Chou <charlottechou8@gmail.com>
2021-12-21 23:29:40 +0800

    修改飯店頁面+飯店照片

--- EpidemicHelper/resources/views/hotel.blade.php
+++ EpidemicHelper/resources/views/hotel.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
         <link rel="stylesheet" type="text/css" href="../css/general.css">
         <link rel="stylesheet" type="text/css" href="../css/hotel.css">
-        <!-- <link rel="stylesheet" type="text/css" href="../css/bootstrap.css"> -->
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
         <title>台北/新北/基隆 旅館清單</title>
 
     </head>
@@ -25,42 +26,49 @@
             <div class="hotel_row">
                 <div class="hotel">
                     <a href="https://asiayo.app.link/WYrxntgtYgb?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_1.png" alt="Hotel_1" class="hotel_image">
+                        <img src="../img/hotel_1.jpg" alt="Hotel_1" class="hotel_image">
                     </a>
                     <h4 class="hotel_name">AAA旅館</h4>
                 </div>
                 <div class="hotel">
                     <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39640/?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_1.png" alt="Hotel_1" class="hotel_image">
+                        <img src="../img/hotel_2.jpg" alt="Hotel_1" class="hotel_image">
                     </a>
                     <h4 class="hotel_name">BBB旅館</h4>
                 </div>
                 <div class="hotel">
                 <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_1.png" alt="Hotel_1" class="hotel_image">
+                        <img src="../img/hotel_3.jpg" alt="Hotel_1" class="hotel_image">
                     </a>
                     <h4 class="hotel_name">CCC旅館</h4>
                 </div>
             </div>
-            
             <div class="hotel_row">
                 <div class="hotel">
                     <a href="https://asiayo.app.link/WYrxntgtYgb?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_1.png" alt="Hotel_1" class="hotel_image">
+                        <img src="../img/hotel_1.jpg" alt="Hotel_1" class="hotel_image">
                     </a>
                     <h4 class="hotel_name">AAA旅館</h4>
                 </div>
                 <div class="hotel">
-                <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39640/?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_1.png" alt="Hotel_1" class="hotel_image">
+                    <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39640/?appdownload=on&aff_id=291&people=1">
+                        <img src="../img/hotel_2.jpg" alt="Hotel_1" class="hotel_image">
                     </a>
                     <h4 class="hotel_name">BBB旅館</h4>
                 </div>
                 <div class="hotel">
                 <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_1.png" alt="Hotel_1" class="hotel_image">
+                        <img src="../img/hotel_3.jpg" alt="Hotel_1" class="hotel_image">
                     </a>
                     <h4 class="hotel_name">CCC旅館</h4>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="../img/hotel_3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
         </div> 
