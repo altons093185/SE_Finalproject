7bcba46d34f2051c362e272549da1d14624c7fe5
Charlotte Chou <charlottechou8@gmail.com>
2021-12-23 05:26:04 +0800

    修改菜單、home、hotel

--- EpidemicHelper/resources/views/hotel.blade.php
+++ EpidemicHelper/resources/views/hotel.blade.php
@@ -1,3 +1,11 @@
+<!-- 指定繼承 layout.master 母模板 -->
+@extends('layout.default')
+
+<!-- 傳送資料到母模板，並指定變數為title -->
+
+<!-- 傳送資料到母模板，並指定變數為content -->
+
+@section('content')
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,70 +17,85 @@
 
     </head>
     <body>
-        <header class="head">
-        <img src="../icon/logo.png" width="200px">  <!-- 點擊可以跳回首頁 -->
-        <div class="module_pages">
-            <button class="flight">航班</button>
-            <button class="serverity_level">防疫等級</button>
-            <button class="hotel">旅館</button>
-            <button class="login_logout">登入</button> <!-- 1.登出後 2.跳回首頁 -->
-        </div>
-        </header>
 
         <h3 class="page_title">台北/新北/基隆 旅館清單</h3>
         <h4 class="page_description">瀏覽防疫旅館</h4>
 
+        <!-- 飯店列表 -->
         <div class="hotel_display">
+
+            <!-- 一橫排的飯店 -->
             <div class="hotel_row">
-                <div class="hotel">
-                    <a href="https://asiayo.app.link/WYrxntgtYgb?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_1.jpg" alt="Hotel_1" class="hotel_image">
+
+                <!-- 一間飯店 -->
+                <div class="card" style="width: 18rem;">
+                    <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1">
+                        <img src="../img/hotel_3.jpg" alt="Hotel_3" class="card-img-top">
                     </a>
-                    <h4 class="hotel_name">AAA旅館</h4>
+                    <div class="card-body">
+                        <h5 class="card-title">AAA旅館</h5>
+                        <p class="card-text">台北</p>
+                    </div>
                 </div>
-                <div class="hotel">
-                    <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39640/?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_2.jpg" alt="Hotel_1" class="hotel_image">
+
+                <div class="card" style="width: 18rem;">
+                    <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1">
+                        <img src="../img/hotel_3.jpg" alt="Hotel_3" class="card-img-top">
                     </a>
-                    <h4 class="hotel_name">BBB旅館</h4>
+                    <div class="card-body">
+                        <h5 class="card-title">AAA旅館</h5>
+                        <p class="card-text">台北</p>
+                    </div>
                 </div>
-                <div class="hotel">
-                <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_3.jpg" alt="Hotel_1" class="hotel_image">
+
+                <div class="card" style="width: 18rem;">
+                    <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1">
+                        <img src="../img/hotel_3.jpg" alt="Hotel_3" class="card-img-top">
                     </a>
-                    <h4 class="hotel_name">CCC旅館</h4>
+                    <div class="card-body">
+                        <h5 class="card-title">AAA旅館</h5>
+                        <p class="card-text">台北</p>
+                    </div>
                 </div>
+
             </div>
+
+            <!-- 一橫排的飯店 -->
             <div class="hotel_row">
-                <div class="hotel">
-                    <a href="https://asiayo.app.link/WYrxntgtYgb?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_1.jpg" alt="Hotel_1" class="hotel_image">
+                
+                <div class="card" style="width: 18rem;">
+                    <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1">
+                        <img src="../img/hotel_3.jpg" alt="Hotel_3" class="card-img-top">
                     </a>
-                    <h4 class="hotel_name">AAA旅館</h4>
+                    <div class="card-body">
+                        <h5 class="card-title">AAA旅館</h5>
+                        <p class="card-text">台北</p>
+                    </div>
                 </div>
-                <div class="hotel">
-                    <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39640/?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_2.jpg" alt="Hotel_1" class="hotel_image">
+
+                <div class="card" style="width: 18rem;">
+                    <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1">
+                        <img src="../img/hotel_3.jpg" alt="Hotel_3" class="card-img-top">
                     </a>
-                    <h4 class="hotel_name">BBB旅館</h4>
+                    <div class="card-body">
+                        <h5 class="card-title">AAA旅館</h5>
+                        <p class="card-text">台北</p>
+                    </div>
                 </div>
-                <div class="hotel">
-                <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1">
-                        <img src="../img/hotel_3.jpg" alt="Hotel_1" class="hotel_image">
+
+                <div class="card" style="width: 18rem;">
+                    <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1">
+                        <img src="../img/hotel_3.jpg" alt="Hotel_3" class="card-img-top">
                     </a>
-                    <h4 class="hotel_name">CCC旅館</h4>
+                    <div class="card-body">
+                        <h5 class="card-title">AAA旅館</h5>
+                        <p class="card-text">台北</p>
+                    </div>
                 </div>
-            </div>
-            <div class="card" style="width: 18rem;">
-                <img src="../img/hotel_3.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="https://asiayo.com/zh-tw/view/tw/taipei-city/39291/?appdownload=on&aff_id=291&people=1" class="btn btn-primary">Go somewhere</a>
-                </div>
+
             </div>
         </div> 
 
     </body>
 </html>
-
+@endsection
--- EpidemicHelper/resources/views/includes/header.blade.php
+++ EpidemicHelper/resources/views/includes/header.blade.php
@@ -1,36 +1,54 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="search">Search</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="company">company</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+        <!-- 菜單 --> 
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+
+                <!-- logo -->
+                <nav class="navbar navbar-light bg-light">
+                    <a class="navbar-brand" href="../public">
+                        <img src="../resources/icon/logo.png" height="30" class="d-inline-block align-top" alt="">
+                    </a>
+                </nav>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                        <li class="nav-item active">
+                            <a class="nav-link" href="../public">首頁</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                航班資訊
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href=" flightSearch.blade.php">航班查詢</a></li>
+                                <li><a class="dropdown-item" href="../resources/views/flightManagement.blade.php">航班管理</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                防疫等級資訊
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">防疫等級清單</a></li>
+                                <li><a class="dropdown-item" href="../resources/views/severityLevelManagement.blade.php">防疫等級管理</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                防疫旅館資訊
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="../resources/views/hotel.blade.php">台北/新北/基隆旅館</a></li>
+                                <li><a class="dropdown-item" href="../resources/views/hotel.blade.php">桃園/新竹/台中旅館</a></li>
+                                <li><a class="dropdown-item" href="../resources/views/hotel.blade.php">台南/高雄旅館</a></li>
+                                <li><a class="dropdown-item" href="../resources/views/hotelManagement.blade.php">旅館管理</a></li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
